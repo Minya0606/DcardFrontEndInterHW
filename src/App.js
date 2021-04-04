d681901f59fe3902ea68042dea1c789fb663246f
Minya <carter850065@gmail.com>
2021-04-04 23:12:30 +0800

    complete v1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Scienc from './components/Scienc'
 import SciencCity from './components/City.js'
-import {Navbar, Nav, Button} from 'react-bootstrap'
+import {Navbar, Nav, Container, Row, Col} from 'react-bootstrap'
 
 function App(){
   return(
@@ -9,22 +9,17 @@ function App(){
       <Navbar bg="light" expand="lg">
       <Link to="/" className="navbar-brand"> Demo </Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Link to="/scienc" className="nav-link">全部景點</Link>
-          <Link to="/sciencCity" className="nav-link">城市景點</Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-    <Route exact path="/"> <Home /> </Route>
-    <Route path="/scienc"> <Scienc /> </Route>
-    <Route path="/sciencCity" component={SciencCity}></Route>
-      {/* <Switch>
-          <Route exact path="/">
-          </Route>
-          <Route path="/scenic"><Test1 /></Route>
-          <Route path="/scenicCity" component={Test2}/>
-      </Switch> */}
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Link to="/sciencSpot" className="nav-link">全部景點</Link>
+            <Link to="/sciencCity" className="nav-link">城市景點</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Route exact path="/"> <Home /> </Route>
+      <Route path="/sciencSpot"> <Scienc /> </Route>
+      <Route path="/sciencCity" component={SciencCity} />
+      
     </div>
      
   )
@@ -33,8 +28,24 @@ function App(){
 function Home (){
   return (
     <div width="100%" align="center">
-      Welcome
-      <Button variant="outline-secondary">I</Button>
+      <Container style={{"marginTop":"30px"}}>
+        <Row className="justify-content-md-center">
+            <Col>
+             <h2>Welcome</h2>
+            </Col>
+        </Row>
+        <Row className="justify-content-md-center justify-content-sm-center" style={{"marginBottom":"5px"}}>
+          <Col sm={6} md={6}>
+            <Link to="/sciencSpot" className="btn btn-outline-secondary" style={{"width":"100%"}}>瀏覽全部景點</Link>
+          </Col>
+        </Row>
+        <Row className="justify-content-md-center justify-content-sm-center" style={{"marginBottom":"5px"}}>
+          <Col sm={6} md={6}>
+            <Link to="/sciencCity" className="btn btn-outline-secondary" style={{"width":"100%"}}>瀏覽縣市景點</Link>
+          </Col>
+        </Row>
+      </Container>
+      
     </div>
   )
 }
